89b109472572a0eae8d3c95dea2832d8f29851c9
Thet Tun <thettun.loard17@gmail.com>
2024-11-03 17:29:20 +0700

    fixed some desing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,16 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     fontSize: {
       sm: [
         "13px",
